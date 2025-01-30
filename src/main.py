40dcf76fb26c54d4aa860661bb4d494044db5368
Sam Khan <psam1625@gmail.com>
2025-01-30 14:22:22 -0500

    Basic logic of scraper

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import html
 import requests
 # Get web page link
-html_content = requests.get("https://requests.readthedocs.io/en/latest/")
+html_content = requests.get("https://tracker.gg/valorant")   
 # Create a BeautifulSoup object
-soup = BeautifulSoup(html_content, "html.parser")
+soup = BeautifulSoup(html_content.text, "html.parser")
+print(soup.prettify)
